35a58a0658f01297f7b4f675ff5396567396eb53
valorad <valorad@outlook.com>
2019-04-06 17:31:04 -0400

    add title

--- elec242-workshop4/q1.m
+++ elec242-workshop4/q1.m
@@ -12,6 +12,9 @@ x = square(t)/2;
 
 figure(1)
 plot(t, x)
+title("x(t)")
+xlabel('t')
+ylabel('x(t)')
 
 n = t;
 
@@ -19,6 +22,9 @@ g5 = fEstimate(n, 5);
 
 figure(2)
 stem(n, g5)
+title("g(t) when N = 5")
+xlabel('t')
+ylabel('g(t)')
 
 mse5 = sqrt((x - g5).^2);
 % Log the max mse out in console
@@ -34,11 +40,17 @@ max(mse5)
 g10 = fEstimate(n, 10);
 figure(4)
 stem(n, g10)
+title("g(t) when N = 10")
+xlabel('t')
+ylabel('g(t)')
 mse10 = sqrt((x - g10).^2);
 max(mse10)
 
 g50 = fEstimate(n, 50);
 figure(5)
 stem(n, g50)
+title("g(t) when N = 50")
+xlabel('t')
+ylabel('g(t)')
 mse50 = sqrt((x - g50).^2);
 max(mse50)
