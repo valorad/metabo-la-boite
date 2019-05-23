7c59aa93aaca0aabcae603fc7c7054fe7b6a9164
valorad <valorad@outlook.com>
2019-05-22 23:03:45 -0400

    a simple way to tell linearity

---
+++ elec342/lab2/functions/isLinear.m
@@ -0,0 +1,24 @@
+function [result] = isLinear(func, x1, x2)
+%ISLINEAR Tell if a signal is linear
+%   returns true if is linear
+
+  if nargin < 2
+    x1 = (0: 1: 5);
+  end
+
+  if nargin < 3
+    x2 = (0: 2: 10);
+  end
+
+  result = false;
+
+  a = 10;
+  b = 5;
+
+  if (a .* func(x1) + b .* func(x2) == func(a .* x1 + b .* x2))
+    result = true;
+  end
+  
+end
+  
+  
