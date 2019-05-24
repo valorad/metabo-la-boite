003bf5adba9030a932ca638cad89eef44561357d
valorad <valorad@outlook.com>
2019-05-23 20:17:47 -0400

    some works on q3

--- elec342/lab2/part1/q3.m
+++ elec342/lab2/part1/q3.m
@@ -2,14 +2,26 @@ clear
 clc
 close all
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+% part a)
+%%%%%%%%%%%%%%%%%%%%%%%%%%
 % x[n] = e ^ (-2|n|) * sin( (2*pi/36)n) , 0 <= n <= 10
 
 n1 = (0: 10);
 x1_n = exp( (-2 .* abs(n1)) .* sin( 2 * pi .* n1 / 36 ) );
 
 % get the mirror image
-n1_xflip = (-1) .* fliplr(n1);
+% n1_xflip = (-1) .* fliplr(n1);
 x1_minusN = fliplr(x1_n);
+% x1_minusN = exp( (-2 .* abs(n1_xflip)) .* sin( 2 * pi .* n1_xflip / 36 ) );
+% fill up 0s for both in order to plot later
+% n1_xflip = advance(n1_xflip, 10);
+
+% fill up 0s for both in order to plot later
+n1 = delay(n1, 10);
+n1_flip = (-1) .* fliplr(n1);
+x1_n = delay(x1_n, 10);
+x1_minusN = advance(x1_minusN, 10);
 
 % get the even and odd parts
 x1_n_even = (x1_n + x1_minusN) / 2;
@@ -18,7 +30,7 @@ x1_n_odd = (x1_n - x1_minusN) / 2;
 % plot them out
 figure(1)
 subplot(2, 2, 1)
-plot(n1_xflip, x1_minusN)
+plot(n1_flip, x1_minusN)
 
 title("x[-n]")
 xlabel('n')
@@ -31,13 +43,67 @@ title("x[n]")
 xlabel('n')
 ylabel('x')
 
+subplot(2, 2, 3)
+plot((-10: 10), x1_n_even)
 
+title("even part")
+xlabel('n')
+ylabel('x')
 
+subplot(2, 2, 4)
+plot((-10: 10), x1_n_odd)
+
+title("odd part")
+xlabel('n')
+ylabel('x')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+% part b)
+%%%%%%%%%%%%%%%%%%%%%%%%%%
 % x[n] = (-1) ^ (n-1)
 n2 = (-5: 5);
-x2 = (-1) .^ (n2 - 1);
+x2_n = (-1) .^ (n2 - 1);
 
+% get the mirror image
+n2_xflip = (-1) .* fliplr(n2);
+x2_minusN = fliplr(x2_n);
+
+% fill up 0s for both in order to plot later
+% n2 = delay(n1, 10);
+n2_flip = (-1) .* fliplr(n2);
+% x2_n = delay(x2_n, 10);
+% x2_minusN = advance(x2_minusN, 10);
+
+% get the even and odd parts
+x2_n_even = (x2_n + x2_minusN) / 2;
+x2_n_odd = (x2_n - x2_minusN) / 2;
+
+% plot them out
 figure(2)
 subplot(2, 2, 1)
-plot(x2)
+plot(n2_flip, x2_minusN)
 
+title("x[-n]")
+xlabel('n')
+ylabel('x')
+
+subplot(2, 2, 2)
+plot(n2, x2_n)
+
+title("x[n]")
+xlabel('n')
+ylabel('x')
+
+subplot(2, 2, 3)
+plot((-5: 5), x2_n_even)
+
+title("even part")
+xlabel('n')
+ylabel('x')
+
+subplot(2, 2, 4)
+plot((-5: 5), x2_n_odd)
+
+title("odd part")
+xlabel('n')
+ylabel('x')
