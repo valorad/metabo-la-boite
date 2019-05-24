ef4213098cbf4ddc771736416717b88e058e1f46
valorad <valorad@outlook.com>
2019-05-24 14:40:13 -0400

    plugin the code for part c of q3

--- elec342/lab2/part1/q3.m
+++ elec342/lab2/part1/q3.m
@@ -107,3 +107,34 @@ plot((-5: 5), x2_n_odd)
 title("odd part")
 xlabel('n')
 ylabel('x')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+% part c)
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+figure(3)
+
+% T. Obuchowicz
+%Fri Apr 27 16:03:27 EDT 2012
+
+clear
+n = [1 : 20];
+x1 = sin((2*pi/40) * n) .* cos((2*pi/40) * n);
+
+for index = 1 : 20
+    % Note: In MATLAB, no need to pre-allocate the array,
+    % unlike C++ and other high-level programming languages.
+    x2(index) = sin((2*pi/40) * index) * cos((2*pi/40) * index);
+end
+
+subplot(2,1,1)
+stem(n, x1)
+title('Elegant method making full use of MATLABs array capabilities')
+xlabel('n')
+ylabel('x[n]')
+
+subplot(2,1,2)
+stem(n, x2)
+title('Gets the job done, but it is a lot of work and we are not in the MATLAB mindset')
+xlabel('n')
+ylabel('x[n]')
