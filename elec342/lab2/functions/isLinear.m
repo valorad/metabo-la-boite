72514e62899a2d76f61fe46f8051553f1139250d
valorad <valorad@outlook.com>
2019-05-24 14:38:49 -0400

    fix: some input precisions exceed matlab detection

--- elec342/lab2/functions/isLinear.m
+++ elec342/lab2/functions/isLinear.m
@@ -2,12 +2,13 @@ function [result] = isLinear(func, x1, x2)
 %ISLINEAR Tell if a signal is linear
 %   returns true if is linear
 
+
   if nargin < 2
     x1 = (0: 1: 5);
   end
 
   if nargin < 3
-    x2 = (0: 2: 10);
+    x2 = 2 * x1;
   end
 
   result = false;
@@ -15,7 +16,7 @@ function [result] = isLinear(func, x1, x2)
   a = 10;
   b = 5;
 
-  if (a .* func(x1) + b .* func(x2) == func(a .* x1 + b .* x2))
+  if (a .* func(x1) + b .* func(x2) - func(a .* x1 + b .* x2) <= 0.000000001)
     result = true;
   end
   
