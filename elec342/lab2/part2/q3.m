88e2ab7a89f632922510e4c3709cfe3e4b240773
valorad <valorad@outlook.com>
2019-05-26 15:08:07 -0400

    redesigned q3 in part2

--- elec342/lab2/part2/q3.m
+++ elec342/lab2/part2/q3.m
@@ -2,14 +2,53 @@ clear
 clc
 close all
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+% input A
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 n = (0: 10);
 
 x = 2 * sin(n * pi / 3 + pi / 4);
-y = Sys1(x, n);
+
 
 % parameter used to test Linearity
 x1 = 5 * x;
 x2 = x / 3;
+
+y1 = Sys1(x1, n);
+y2 = Sys1(x2, n);
+
+% plot them out
+figure(1)
+subplot(2, 2, 1)
+stem(x1)
+
+title("x_1[n]= 10sin(n * pi / 3 + pi / 4)")
+xlabel('n')
+ylabel('x1')
+
+subplot(2, 2, 2)
+stem(y1)
+
+title("y_1[n] = n * x_1[n - 1]")
+xlabel('n')
+ylabel('y1')
+
+subplot(2, 2, 3)
+stem(x2)
+
+title("x_2[n]= (2/3)sin(n * pi / 3 + pi / 4)")
+xlabel('n')
+ylabel('x2')
+
+subplot(2, 2, 4)
+stem(y2)
+
+title("y_2[n] = n * x_2[n - 1]")
+xlabel('n')
+ylabel('y2')
+
+disp("input A:")
 
 if isLinear(@Sys1, x1, x2)
 	disp("Outputs consistent with a linear system")
@@ -21,12 +60,231 @@ end
 k0 = 4;
 
 % verify tI
+[tiResult, yShiftXOnly, yShift] = isTimeInvDetail(@Sys1, n, x, k0);
 
-if isTimeInv(@Sys1, n, x, k0)
+% plot them out
+figure(2)
+subplot(3, 1, 1)
+stem(x1)
+
+title("x_1[n]= 10sin(n * pi / 3 + pi / 4)")
+xlabel('n')
+ylabel('x1')
+
+
+subplot(3, 1, 2)
+stem(yShiftXOnly)
+
+title("y_1[n] = n * x_1[n - 1]")
+xlabel('n')
+ylabel('yShiftXOnly')
+
+
+subplot(3, 1, 3)
+stem(yShift)
+
+title("y_1[n] = (n - 1) * x_1[n - 1]")
+xlabel('n')
+ylabel('yShift')
+
+if (tiResult)
 	disp("This system is time-invariant")
 else
     disp("This system is NOT time-invariant")
 end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+% input B
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+n = (0: 10);
+
+x = 2 * sin(n * pi / 3 + pi / 4) ./ (n * pi / 3 + pi / 4) + 8;
+
+% parameter used to test Linearity
+x1 = 5 * x;
+x2 = x / 3;
+
+y1 = Sys1(x1, n);
+y2 = Sys1(x2, n);
+
+% plot them out
+figure(3)
+subplot(2, 2, 1)
+stem(x1)
+
+title("x_1[n]= 10sinc(n * pi / 3 + pi / 4) + 40")
+xlabel('n')
+ylabel('x1')
+
+
+subplot(2, 2, 2)
+stem(y1)
+
+title("y_1[n] = n * x_1[n - 1]")
+xlabel('n')
+ylabel('y1')
+
+
+subplot(2, 2, 3)
+stem(x2)
+
+title("x_2[n]= (2/3) * sinc(n * pi / 3 + pi / 4) + 8/3")
+xlabel('n')
+ylabel('x2')
+
+
+subplot(2, 2, 4)
+stem(y2)
+
+title("y_2[n] = n * x_2[n - 1]")
+xlabel('n')
+ylabel('y2')
+
+disp("input B:")
+
+if isLinear(@Sys1, x1, x2)
+	disp("Outputs consistent with a linear system")
+else
+    disp("Not linear")
+end
+
+% parameter used to test TI
+k0 = 4;
+
+% verify tI
+[tiResult, yShiftXOnly, yShift] = isTimeInvDetail(@Sys1, n, x, k0);
+
+% plot them out
+figure(4)
+subplot(3, 1, 1)
+stem(x1)
+
+title("x_1[n]= 10sinc(n * pi / 3 + pi / 4) + 40")
+xlabel('n')
+ylabel('x1')
+
+
+subplot(3, 1, 2)
+stem(yShiftXOnly)
+
+title("y_1[n] = n * x_1[n - 1]")
+xlabel('n')
+ylabel('yShiftXOnly')
+
+
+subplot(3, 1, 3)
+stem(yShift)
+
+title("y_1[n] = (n - 1) * x_1[n - 1]")
+xlabel('n')
+ylabel('yShift')
+
+if (tiResult)
+	disp("This system is time-invariant")
+else
+    disp("This system is NOT time-invariant")
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+% input C
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+n = (0: 10);
+
+[~, lengthN] = size(n);
+
+x = ones(1, lengthN);
+
+% parameter used to test Linearity
+x1 = 1 * x;
+x2 = x / 2;
+
+y1 = Sys1(x1, n);
+y2 = Sys1(x2, n);
+
+% plot them out
+figure(5)
+subplot(2, 2, 1)
+stem(x1)
+
+title("x_1[n]= u[n]")
+xlabel('n')
+ylabel('x1')
+
+
+subplot(2, 2, 2)
+stem(y1)
+
+title("y_1[n] = n * x_1[n - 1]")
+xlabel('n')
+ylabel('y1')
+
+
+subplot(2, 2, 3)
+stem(x2)
+
+title("x_2[n]= 0.5u[n]")
+xlabel('n')
+ylabel('x2')
+
+
+subplot(2, 2, 4)
+stem(y2)
+
+title("y_2[n] = n * x_2[n - 1]")
+xlabel('n')
+ylabel('y2')
+
+disp("input C:")
+
+if isLinear(@Sys1, x1, x2)
+	disp("Outputs consistent with a linear system")
+else
+    disp("Not linear")
+end
+
+% parameter used to test TI
+k0 = 4;
+
+% verify tI
+[tiResult, yShiftXOnly, yShift] = isTimeInvDetail(@Sys1, n, x, k0);
+
+% plot them out
+figure(6)
+subplot(3, 1, 1)
+stem(x1)
+
+title("x_1[n]= u[n]")
+xlabel('n')
+ylabel('x1')
+
+
+subplot(3, 1, 2)
+stem(yShiftXOnly)
+
+title("y_1[n] = n * x_1[n - 1]")
+xlabel('n')
+ylabel('yShiftXOnly')
+
+
+subplot(3, 1, 3)
+stem(yShift)
+
+title("y_1[n] = (n - 1) * x_1[n - 1]")
+xlabel('n')
+ylabel('yShift')
+
+if (tiResult)
+	disp("This system is time-invariant")
+else
+    disp("This system is NOT time-invariant")
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+% The system
+%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function systemeMystere1 = Sys1(x, n, shift, cmode)
     % Sys1 - Description
