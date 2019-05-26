00ea08bab3a64d8a1d66ff764675853c2af3dede
valorad <valorad@outlook.com>
2019-05-26 15:07:53 -0400

    playground is playing

--- elec342/lab2/playground.m
+++ elec342/lab2/playground.m
@@ -5,41 +5,51 @@ close all
 
 n = (0: 5);
 
-x1 = 2 * n;
+x1 = 1 * n;
+x2 = 2 * n;
 
-ti = isTimeInv(@getY1, n, x1, 2);
 
-l = isLinear(@getY2);
 
-[~, size] = size(n);
+[result, y1, y2, x12, y12] = isLinearDetail(@bGetY1, x1, x2);
+tes = y1 + y2 - y12;
+res = tes <= 0.000000001;
 
-dn = getDeltaN(size);
-
-function y1 = getY1(n, x, shift, cmode)
-% y = t * x(t)
-
-  n_ramp = getRamp(cmode, shift);
-
-  y1 = n_ramp .* delay(x, shift);
-
+function y1 = bGetY1(x)
+    y1 = x .^ 2;
 end
+% ti = isTimeInv(@getY1, n, x1, 2);
 
-function y2 = getY2(x)
-  % y = x(t) ^ 2
-  y2 = x .^ 2;
-end
+% l = isLinear(@getY2);
 
-function deltaN = getDeltaN(length)
-  % create delta function
-  deltaN = zeros(1, length);
-  deltaN(1) = 1;
-end
+% [~, size] = size(n);
 
-function n_ramp = getRamp(cmode, shift)
-  if cmode == "CALCY"
-      n_ramp = delay(n, shift);
-  else
-      % fill up 0s to ensure equal size
-      n_ramp = advance(n, shift);
-  end
-end
+% dn = getDeltaN(size);
+
+% function y1 = getY1(n, x, shift, cmode)
+% % y = t * x(t)
+
+%   n_ramp = getRamp(cmode, shift);
+
+%   y1 = n_ramp .* delay(x, shift);
+
+% end
+
+% function y2 = getY2(x)
+%   % y = x(t) ^ 2
+%   y2 = x .^ 2;
+% end
+
+% function deltaN = getDeltaN(length)
+%   % create delta function
+%   deltaN = zeros(1, length);
+%   deltaN(1) = 1;
+% end
+
+% function n_ramp = getRamp(cmode, shift)
+%   if cmode == "CALCY"
+%       n_ramp = delay(n, shift);
+%   else
+%       % fill up 0s to ensure equal size
+%       n_ramp = advance(n, shift);
+%   end
+% end
